fe04bc900a063bfd961aaafe0845cc1faba7bc27
Abadi Suryo <abadisuryosetiyo@gmail.com>
2024-07-10 08:56:09 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/eternalenterprise/urunan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eternalenterprise.urunan.urunan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:urunan/app/app.dart';
+import 'package:urunan/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
